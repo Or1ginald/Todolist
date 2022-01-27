bac641d03a8a14bc55c6862fba8b5a2fe0c32103
Or1gianld <superhd@icloud.com>
2022-01-27 16:17:03 +0300

    Added redux dev tools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
 import { appReducer, authReducer, tasksReducer, todDoListsReducer } from 'store';
@@ -10,7 +10,18 @@ export const rootReducer = combineReducers({
   auth: authReducer,
 });
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+// export const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const composeEnhancers =
+  // @ts-ignore
+  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    // @ts-ignore
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })) ||
+  compose;
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
 
 // @ts-ignore
 window.store = store;
